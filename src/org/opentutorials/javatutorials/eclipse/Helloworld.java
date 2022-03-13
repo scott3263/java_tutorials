cdd4fba4891dc4c70059e1e16b8cc89e387ca6f8
scott3263 <scott00@naver.com>
2022-03-13 17:21:16 +0900

    전체 commit TEST

--- src/org/opentutorials/javatutorials/eclipse/Helloworld.java
+++ src/org/opentutorials/javatutorials/eclipse/Helloworld.java
@@ -3,6 +3,6 @@ package org.opentutorials.javatutorials.eclipse;
 public class Helloworld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World"); // a
 	}
 }
