5637ed60d38d15db7b43fcc0360335251a212562
scott <scott@DESKTOP-ULH472N>
2022-03-13 17:33:22 +0900

    notebook에서 전체 commit

--- src/org/opentutorials/javatutorials/variables/IntergerDemo.java
+++ src/org/opentutorials/javatutorials/variables/IntergerDemo.java
@@ -5,9 +5,9 @@ public class IntergerDemo {
 	public static void main(String[] args) {
 		int a;
 		a = 1;
-		System.out.println(a+1);  // 2
+		System.out.println(a+1);  
 		
 		a= 2 ;
-		System.out.println(a+1);  // 3
+		System.out.println(a+1); 
 	}
 }
